e2665724dd0c1e3df67e690b6c32ec13bbb97da0
Pablo González <pagoru@gmail.com>
2017-08-19 20:06:27 +0200

    Fixed processes + metrics

--- metrics.js
+++ metrics.js
@@ -4,8 +4,10 @@ const probe = require('pmx').probe();
 module.exports.init = () => {
 
     global.metric_guilds = probe.counter({
-        name: 'guilds',
-        value: global.bot.guilds.size
+        name: 'guilds'
     });
+
+    for(let i = 0; i < global.bot.guilds.size; i++)
+        metric_guilds.inc();
 
 };
